97b74e5f4bf0ad26fe188bff8d34966490023ca8
Dimitris C <dimmdesign@gmail.com>
2017-07-27 17:31:41 +0100

    Added Initial Demo project

---
+++ Demo/ToggleSwitchDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToggleSwitchDemo
+//
+//  Created by Dimitris C. on 27/07/2017.
+//  Copyright © 2017 Decimal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
